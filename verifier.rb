7179d1370333521fff4a32abd51848a3fd81a646
bricktsre <gwoodward21@gmail.com>
2019-04-09 23:15:16 -0400

    Does stuff with transactions and checks if every block finishes with all valid address balances

--- verifier.rb
+++ verifier.rb
@@ -10,6 +10,7 @@ class Verifier
     @previous_block_num = '-1'
     @previous_hash = '0'
     @previous_time = '-1.-1'
+    @addr_table = Hash.new(0)
   end
 
   def main
@@ -19,13 +20,14 @@ class Verifier
       blk_num, old_hash, trans, time, block_hash = line.split('|')
       block_arr = (line[0...line.rindex('|')]).unpack('U*')
       new_hash = calculate_hash(block_arr)
+      do_transactions(trans)
       check_block(blk_num, old_hash, trans, time, new_hash, block_hash.chomp, line_num)
       @previous_block_num = blk_num
       @previous_hash = new_hash
       @previous_time = time
       line_num += 1
     end
-    puts 'Valid Blockchain'
+    print_addresses
   end
 
   def check_block(blk_num, old_hash, _trans, time, new_hash, block_hash, line)
@@ -36,6 +38,8 @@ class Verifier
     error_cases(line, 3, block_hash, new_hash) if new_hash != block_hash
 
     error_cases(line, 4, time, @previous_time) unless check_time(time)
+  
+    check_addresses(line)
   end
 
   def check_time(time)
@@ -43,6 +47,25 @@ class Verifier
     old_seconds, old_nano = @previous_time.split('.')
     return true if seconds.to_i > old_seconds.to_i
     nano.to_i > old_nano.to_i
+  end
+
+  def check_addresses(line)
+    @addr_table.each do |key, value|
+	    error_cases(line, 5, value, key) if value < 0
+    end
+  end
+
+  def do_transactions(trans)
+    trans_table = trans.split(':')
+    trans_table.each do |transaction|
+      sender, reciever = transaction.split(">")
+      amt = reciever[(reciever.rindex('(')+1)...reciever.rindex(')')].to_i
+      reciever = reciever[0...reciever.rindex('(')]
+      if sender != 'SYSTEM'
+        @addr_table[sender] = @addr_table[sender] - amt
+      end
+      @addr_table[reciever] = @addr_table[reciever] + amt
+    end
   end
 
   def calculate_hash(block)
@@ -57,6 +80,12 @@ class Verifier
     ((x**3000) + (x**x) - (3**x)) * (7**x)
   end
 
+  def print_addresses
+	  @addr_table.sort.map do |key, value|
+	    puts "#{key}: #{value} billcoins" if value > 0
+    end
+  end
+
   def error_cases(line, error_num, value, expected)
     case error_num
     when 0
@@ -69,6 +98,8 @@ class Verifier
       puts "Line #{line}: Current hash is #{value}, should be #{expected}"
     when 4
       puts "Line #{line}: New timestamp #{value} <= previous #{expected}"
+    when 5 
+      puts "Line #{line}: Address #{expected} has invalid balance of #{value}"
     end
     puts 'BLOCKCHAIN INVALID'
     exit 1
